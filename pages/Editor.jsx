a4906964558e7ae73c7eb7320563fa299826979f
omartrese <108763869+omartrese@users.noreply.github.com>
2025-11-26 22:33:58 +0100

    The fucking drag and drop doesn't work

--- pages/Editor.jsx
+++ pages/Editor.jsx
@@ -1,10 +1,11 @@
 import { View, StyleSheet } from "react-native"
 import CardImage from "../components/CardImage"
-import {useBoard} from "../BoardContext"
+import { useBoard } from "../BoardContext"
+
 
 export default function Editor() {
 
-    const {board} = useBoard();
+    const { board } = useBoard();
 
     return (
         <View style={styles.container}>
